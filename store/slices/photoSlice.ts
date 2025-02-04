5c1090315470cb981e8bad0cbd73a9bff30c0eea
zeriab <zeriab@methics.fi>
2025-02-04 21:14:10 +0200

    store
    plantSlice

---
+++ store/slices/photoSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+
+interface Photo {
+  id: string;
+  uri: string;
+  name: string;
+  notes: string;
+  date: string;
+}
+
+interface PhotoState {
+  photos: Photo[];
+}
+
+const initialState: PhotoState = {
+  photos: [],
+};
+
+const photoSlice = createSlice({
+  name: "photos",
+  initialState,
+  reducers: {
+    addPhoto: (state, action: PayloadAction<Photo>) => {
+      const newPhoto = {
+        ...action.payload,
+        id: uuid(),
+      };
+
+      state.photos.push(newPhoto);
+    },
+    updatePhoto: (state, action: PayloadAction<Photo>) => {
+      const index = state.photos.findIndex((p) => p.id === action.payload.id);
+      console.log("index is " + index);
+      if (index !== -1) {
+        state.photos[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { addPhoto, updatePhoto } = photoSlice.actions;
+export const selectPhotos = (state: any) => state.photos.photos;
+export default photoSlice.reducer;
